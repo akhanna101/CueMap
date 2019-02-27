8ba7f6fef62be9848ff1603ba9df2315e3d04783
akhanna101 <akhanna101@gmail.com>
2019-02-27 16:03:18 -0500

    Revert "updated CM_v1"

--- CMv1.py
+++ CMv1.py
@@ -1,14 +1,72 @@
-
+import numpy as np
 from pygame import *
 import pygame, time, numpy, pygame.sndarray
+import time as time
+import RPi.GPIO as GPIO
 
+
+global NPs
+
+NPs = ([], [])
+
+#Input output pins for the dipper and nosepoke
+dipper = 23
+nosePoke = 11
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(nosePoke, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def IOD(a):
+    
+    if GPIO.input(nosePoke):
+        print('ON')
+        
+        NPs[0].append(time.time())
+##        tf = open("TT","a")
+##        tf.write(str(time.time()) + "   " + "F" + '\n')
+##        
+    else:
+        print('OFF')
+        NPs[1].append(time.time())
+##        tf = open("TT","a")
+##        tf.write(str(time.time()) + "   " + "R" + '\n')
+  
+  
+GPIO.add_event_detect(nosePoke, GPIO.BOTH, callback=IOD, bouncetime=100)
+GPIO.setup(dipper, GPIO.OUT) # UPDATE PIN OUT FOR DIPPER
+GPIO.output(dipper, 0)
+
+cueTime_ms = 4000
 sample_rate = 44100
 bits = 16
 max_sample = 2**(bits - 1) - 1
 
 pixels = 12
 
-Map = numpy.empty((pixels,2),dtype=object)
+Map = numpy.empty((pixels,2),dtype=object)  
+
+
+def savedata(Event):
+    global NPs
+    t = time.time()
+    tf = open(filename,"a")
+    for x in NPs[0]:
+        
+        tf.write(str(x-st) + "   " + "F" + '\n')
+        
+    for x in NPs[1]:
+        
+        tf.write(str(x-st) + "   " + "R" + '\n')
+        
+    NPs = ([],[])
+    
+    tf.write(str(t-st) + "   " + Event + '\n')
+        
+    tf.close()
+    
+    
+
+
 
 def getsin(sample_rate,freq,max_sample):
     length = sample_rate / float(freq)
@@ -27,7 +85,7 @@ def getclick(sample_rate,freq,max_sample):
     #return (numpy.int16(max_sample * numpy.sin(xvalues)))
     return (numpy.stack((f, f),axis=1))
 
-def play_for(sample_array_r,sample_array_c, ms, volLeft, volRight):
+def play_for(sample_array_r,sample_array_c, volLeft, volRight):
     soundr = pygame.sndarray.make_sound(sample_array_r)
     soundc = pygame.sndarray.make_sound(sample_array_c)
     #beg = time.time()
@@ -42,7 +100,7 @@ def play_for(sample_array_r,sample_array_c, ms, volLeft, volRight):
     #channelc.set_volume(volLeft,0)
     pygame.mixer.Channel(2).set_volume(volLeft,0)
     
-    pygame.time.delay(ms)
+    #pygame.time.delay(ms)
     #sound.stop()
     
 def trunc_divmod(a, b):
@@ -50,6 +108,32 @@ def trunc_divmod(a, b):
     q = -int(-q) if q<0 else int(q)
     r = a - b * q
     return q, r
+
+def trajectoryInput():
+    animal = input("What animal is this?")
+    day = input("What day of training is this?")
+    filename_save = str(animal) + str(day) + '.txt' # Determine the filename for the traininglist trajectory
+    
+    filename_in = 'Lists_' + str(animal) + '_' + str(day) + '.txt' # Determine the filename for the traininglist trajectory
+    
+    with open (filename_in, 'r') as f:
+        training_list = f.readlines()
+        #training_list = [x.strip() for x in training_list] 
+        #This opens the trajectory list file for that day and turns it into a list
+    
+    #Find the Rewarded Pixels based on the pixels that fall between % in our training list.
+    rewardIndices = []
+    for i, elem in enumerate(training_list):
+        if '%' in elem:
+            rewardIndices.append(i)
+    rew = training_list[((rewardIndices[0])+1):rewardIndices[1]]
+    rew = list(map(int,rew))
+    
+    #Separates trajectory list from reward list and turns it into pos. pos is a list of integers indicating which pixels to play
+    pos = training_list[((rewardIndices[1])+1):-1]
+    pos = list(map(int, pos))
+    
+    return (filename_save, rew, pos)
 
 
 #This fills the tones first
@@ -63,31 +147,42 @@ for p in range(pixels):
     freq = freq*spacing
     cfreq = cfreq*spacing
     
-print(Map[0][0].shape)
+print(Map[3][0].shape)
 print(Map[3][0].shape)
 pygame.mixer.pre_init(sample_rate, -16, 2) # 44.1kHz, 16-bit signed, stereo
 pygame.init()
 
 _running = True
 
-pos = [1,2,14,15,27,132,144]
+filename, rew, pos = trajectoryInput()
+
+tf = open(filename,"w")
+tf.write('START' +'\n')
+tf.close()
+
+
 volt = [0.8,0.5,0.45,0.42,0.4,0.4,0.4,0.42,0.45,0.5,0.6,0.7]
 volc = [1,1,0.99,0.98,0.97,0.95,0.93,0.90,0.87,0.83,0.79,0.74]
 
-n = 0
-#while _running:
+st = time.time()
+
 for p in pos:    
     start = time.time()
-    c,r = trunc_divmod(p-1,12)
+    c,r = trunc_divmod(p-1,pixels)
     #play_for(numpy.stack((Map[r][0], Map[c][1]),axis=1),4000,0.5,0.5)
-    play_for(Map[r][0],Map[c][1],4000,.05*volc[c],.02*volt[r])
+    play_for(Map[r][0],Map[c][1],volc[c],volt[r])
+    savedata(str(p))
     #play_for(Map[c][1],4000,1,0)
     print(time.time()-start)
-    #n = n+1
-         
+    if p in rew:  
+        GPIO.output(dipper, 1)
+    pygame.time.delay(cueTime_ms)
+    GPIO.output(dipper, 0)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             _running = False
             break
-        
+     
+savedata("end")      
 pygame.quit()    
