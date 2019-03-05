bea58a39597d8d6eee06fce6e0325a694a17da4c
SchoenRats <matthew.gardner2@nih.gov>
2019-03-05 12:01:43 -0500

    updated the file saving in  'Feeder Shaping.py'

--- Feeder Shaping.py
+++ Feeder Shaping.py
@@ -2,10 +2,11 @@ import time as time
 import RPi.GPIO as GPIO
 import random
 
-nosePokePin = 27
+nosePokePin = 22
 feederPin = 23
 global NPs
-NPs = ([],[])  
+#NPs = ([],[])  
+NPs = [];
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(feederPin, GPIO.OUT)
@@ -20,23 +21,33 @@ filename = str(animal) + '_' + str(date)+ '_' + str(day) + '.txt'
 
 def NosePoke(a):
     if GPIO.input(nosePokePin):
-        NPs[0].append('on')
-        NPs[1].append(time.time())
+        NPs.append('on' + '   ' + str(time.time() - st))
+        #NPs[1].append(time.time())
     else:
-        NPs[0].append('off')
-        NPs[1].append(time.time())
+        NPs.append('off' + '   ' + str(time.time() - st))
+        #NPs[1].append(time.time())
 
-GPIO.add_event_detect(nosePokePin, GPIO.BOTH, callback=NosePoke, bouncetime=100)
+GPIO.add_event_detect(nosePokePin, GPIO.BOTH, callback=NosePoke, bouncetime=10)
+
+def savedata():
+    global NPs
+    tf = open(filename,"w")
+    for x in NPs:
+        
+        tf.write(x + '\n')
+        
+    tf.close
+
+st = time.time()
 
 for i in range(40):
-    time.sleep(random.randint(1,3))
+    time.sleep(random.randint(60,180))
     GPIO.output(feederPin,1)
     time.sleep(1)
     GPIO.output(feederPin,0)
 
-pokeData = open(filename,"w")
-pokeData.write(NPs)
-pokeData.close()
+##pokeData = open(filename,"w")
+##pokeData.write(str(NPs))
+##pokeData.close()
+savedata()
 
-len(NPs[0])
-exit()
