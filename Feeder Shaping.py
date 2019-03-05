f7ca0f60ef7494802720bb145e4b4fd6b61ea05b
SchoenRats <matthew.gardner2@nih.gov>
2019-03-05 11:13:50 -0500

    Added argument to NosePoke function to address "takes 0 positional arguments" error

--- Feeder Shaping.py
+++ Feeder Shaping.py
@@ -2,32 +2,34 @@ import time as time
 import RPi.GPIO as GPIO
 import random
 
-feederPin = 
-global NPs = ([],[])  
+nosePokePin = 27
+feederPin = 23
+global NPs
+NPs = ([],[])  
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(feederPin, GPIO.OUT)
 GPIO.output(feederPin,0)
 
 GPIO.setup(nosePokePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(noesPokePin, GPIO.BOTH, callback=NosePoke, bouncetime=100)
 
 animal = input("What animal is this?")
 date = input("What is today's date?")
 day = input("What day of training is this?")
 filename = str(animal) + '_' + str(date)+ '_' + str(day) + '.txt'
 
-
-def NosePoke:
-    if GPIO.input(nosePokePin, 1):
+def NosePoke(a):
+    if GPIO.input(nosePokePin):
         NPs[0].append('on')
         NPs[1].append(time.time())
     else:
         NPs[0].append('off')
         NPs[1].append(time.time())
 
+GPIO.add_event_detect(nosePokePin, GPIO.BOTH, callback=NosePoke, bouncetime=100)
+
 for i in range(40):
-    time.sleep(random.randint(60,180))
+    time.sleep(random.randint(1,3))
     GPIO.output(feederPin,1)
     time.sleep(1)
     GPIO.output(feederPin,0)
